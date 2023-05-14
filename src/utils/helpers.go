9afbc4002ea3641ab31c54f1fba85886c3636069
Xukay101 <zaca03zaca@gmail.com>
2023-05-14 00:47:39 -0300

    Update helpers.go

--- src/utils/helpers.go
+++ src/utils/helpers.go
@@ -1,9 +1,26 @@
 package utils
 
-import "log"
+import (
+	"log"
+)
 
 func FatalOnError(message string, err error) {
 	if err != nil {
 		log.Fatalf("%s: %v", message, err)
+	}
+}
+
+func PanicOnError(message string, err error) {
+	if err != nil {
+		log.Panicf("%s: %v", message, err)
+	}
+}
+
+func PrintOnError(message string, err error) bool {
+	if err != nil {
+		log.Printf("%s: %v", message, err)
+		return true
+	} else {
+		return false
 	}
 }
