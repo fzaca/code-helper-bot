0a7be357de225823165ac4ed9cc85aaf12d1089d
Xukay101 <zaca03zaca@gmail.com>
2023-05-17 14:44:43 -0300

    Update database package

--- src/database/database.go
+++ src/database/database.go
@@ -3,17 +3,42 @@ package database
 import (
 	"database/sql"
 	"fmt"
+	"log"
 
+	"github.com/Xukay101/code-helper-bot/src/utils"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func getDb() (*sql.DB, error) {
-	conn, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s",
+	connString := fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s?parseData=True",
 		GetConfig().Database.User,
 		GetConfig().Database.Password,
 		GetConfig().Database.Host,
 		GetConfig().Database.Port,
 		GetConfig().Database.Name,
-	))
-	return conn, err
+	)
+
+	conn, err := sql.Open("mysql", connString)
+	utils.FatalOnError("Error getting database", err)
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return conn, nil
+}
+
+func initDb() {
+	conn, err := getDb()
+	utils.FatalOnError("Error trying to initialize database", err)
+	defer conn.Close()
+
+	for _, instruction := range instructions {
+		_, err := conn.Exec(instruction)
+		utils.FatalOnError("Error starting the database, wrong instruction", err)
+	}
+
+	log.Print("The database started correctly.")
 }
---
+++ src/database/schema.go
@@ -0,0 +1,8 @@
+package database
+
+var instructions = []string{
+	// This is example
+	// "CREATE TABLE IF NOT EXISTS table1 (id INT PRIMARY KEY, name VARCHAR(255))",
+	// "CREATE TABLE IF NOT EXISTS table2 (id INT PRIMARY KEY, age INT)",
+	// Next intructions
+}
